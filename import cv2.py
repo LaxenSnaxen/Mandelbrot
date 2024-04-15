2d8c858bc775585b4e1d78fc84f0808245269eff
Laxen <maximiliam.froste@outlook.com>
2024-04-15 16:47:33 +0200

    Kommentarer

--- import cv2.py
+++ import cv2.py
@@ -15,7 +15,7 @@ def getComplexPlane(start, stop, num):
     xSpace = np.linspace(start[0], stop[0], int(num[0]))
     ySpace = np.linspace(start[1], stop[1], int(num[1]))
     # Meshgrids (det mellan det riktiga och komplexa nummer, en mesh av de båda)
-    real, imag = np.meshgrid(xSpace, ySpace[::-1])
+    real, imag = np.meshgrid(xSpace, ySpace[::-1]) # Meshgrid skapar en grid av de två arrayerna, en array med eindimensionala koordinater.
     # Bygger den komplexa planan
     c = np.zeros_like(real, dtype=np.cdouble)
     c.real = real
@@ -53,56 +53,56 @@ def colourImage(image, mod, cMap=cv2.COLORMAP_TWILIGHT_SHIFTED):
     return image
 
 
-# def textOverlay(image, center, radius, maxIter, resolution):
-#     # Setup för text
-#     h = resolution[1]
-#     fontScale = h / 1300
-#     fontHeight = 30 * fontScale
-#
-#     c = np.around(center, 3)
-#     if radius[1] < 1:
-#         r = [np.format_float_scientific(x, 3, trim="-") for x in radius]
-#     else:
-#         r = np.around(radius, 3)
-#
-#     # Lägger till text
-#     image = cv2.putText(
-#         image,
-#         (
-#             f"center: ({c[0]}, {c[1]}i)     radius: ({r[0]}, {r[1]})    "
-#             + f"resolution: {resolution[0]}x{resolution[1]}    iterations {maxIter}"
-#         ),
-#         (int(0.5 * fontHeight), int(1.5 * fontHeight)),
-#         cv2.FONT_HERSHEY_SIMPLEX,
-#         fontScale,
-#         (255, 255, 255),
-#         thickness=max(1, int(fontScale)),
-#     )
-#     return image
+# Lägger till text på bilden
+def textOverlay(image, center, radius, maxIter, resolution):
+    # Setup för text
+    h = resolution[1]
+    fontScale = h / 1300
+    fontHeight = 30 * fontScale
+    c = np.around(center, 3)
+    if radius[1] < 1:
+        r = [np.format_float_scientific(x, 3, trim="-") for x in radius]
+    else:
+        r = np.around(radius, 3)
+    # Lägger till texten
+    image = cv2.putText(
+        image,
+        (
+            f"center: ({c[0]}, {c[1]}i)     radius: ({r[0]}, {r[1]})    "
+            + f"resolution: {resolution[0]}x{resolution[1]}    iterations {maxIter}"
+        ),
+        (int(0.5 * fontHeight), int(1.5 * fontHeight)),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale,
+        (255, 255, 255),
+        thickness=max(1, int(fontScale)),
+    )
+    return image
 
 if __name__ == "__main__":
-    k = 600
+
+    k = 600 # Påverkar upplösningen, storleken av fönstret. i aspect ratio 16/9 så är 600: 9.
     aspectRatio = 16 / 9
-    resolution = np.array([aspectRatio * k, k], dtype=int)
+    resolution = np.array([aspectRatio * k, k], dtype=int)  # Här räknar programmet ut upplösningen.
 
-    center = np.array([-0.5, 0.0], dtype=float)
-    radius = np.array([aspectRatio, 1], dtype=float)
-    start = center - radius
-    stop = center + radius
+    center = np.array([-0.5, 0.0], dtype=float) # Här är centrumet av bilden
+    radius = np.array([aspectRatio, 1], dtype=float) # Här är radien av bilden
+    start = center - radius # Här är starten av bilden
+    stop = center + radius # Här är slutet av bilden, programmet gör det här för att kolla vart gränserna går.
 
-    maxIter = 40
-    lastMaxIter = 0
-    mod = 40
-    cMap = 19
+    maxIter = 40 # Här är antalet iterationer till at börja med.
+    lastMaxIter = 0 # Här är antalet iterationer från förra gången.
+    mod = 40 # Här är modulot av bilden.
+    cMap = 19 # Här är färgkartan av bilden.
 
-    redoSpace = True
-    redoIter = True
-    redoImage = True
+    redoSpace = True # Om redoSpace är True så gör den om hela skiten, eller skapar den.
+    redoIter = True # Om redoIter är True så gör den om iterationerna.
+    redoImage = True # Om redoImage är True så gör den om bilden.
+    # De olika boolean värdena ovanför ändras beroende på vad användaren gör. Om användaren spara bilden ska den inte slösa 
 
-    key = ord("w")
 
     cv2.namedWindow("Mandelbrot")
-    cv2.setMouseCallback("Mandelbrot", onMouse)
+    cv2.setMouseCallback("Mandelbrot", onMouse) # Kollar vart musen klickar i programmet.
 
     while True:
         if redoSpace == True:
@@ -110,19 +110,19 @@ if __name__ == "__main__":
 
         if redoIter == True:
             escape = applyNTimes(
-                np.zeros_like(c),
+                np.zeros_like(c), # zeros_like skapar en array med samma form som c
                 c,
                 lastMaxIter,
                 maxIter,
-                np.zeros_like(c, dtype=float),
+                np.zeros_like(c, dtype=float), # Här dikterar dtype=float vilken data typ som man får tillbaka av zeros_like
             )
         if redoImage == True:
-            image = colourImage(escape, mod, cMap)
-            cv2.imshow("Mandelbrot", image)
+            image = textOverlay(colourImage(escape, mod, cMap), center, radius, maxIter, resolution) # Här lägger den till texten på bilden och färgar den.
+            cv2.imshow("Mandelbrot", image) # Bilden visas
         print(
-            f"section [[{start[0]},{start[1]}],[{stop[0]},{stop[1]}]]\nmaxiter {maxIter}"
+            f"section [[{start[0]},{start[1]}],[{stop[0]},{stop[1]}]]\nmaxiter {maxIter}" # Information om bilden skrivs ut.
         )
-        print(f"cmap {cMap}\nres {resolution}\n")
+        print(f"cmap {cMap}\nres {resolution}\n") # Ytterligare information skrivs ut.
 
         # Föberedelser för nästa iteration
         redoSpace = True
@@ -130,10 +130,10 @@ if __name__ == "__main__":
         redoImage = True
         lastMaxIter = 0
 
-        key = cv2.waitKey() & 0xFF
+        key = cv2.waitKey() & 0xFF # 0xFF gör så att rätt nummer kommer till wailKey, annars kan det bli fel. För att om man har num lock återvänder olika nummer.
 
         if key == ord("q"):
-            cv2.destroyAllWindows()
+            cv2.destroyAllWindows() # Stänger alla fönster och avslutar loopen.
             break
         elif key == ord("s"):
             x_str = f"{str(start[0]).replace('.','_')}-{str(stop[0]).replace('.','_')}"
@@ -146,13 +146,13 @@ if __name__ == "__main__":
             redo_image = False
 
         elif key == ord("e"):
-            # extend maxiter
+            # Ökar Maxiter, ger fler detaljer.
             lastMaxIter = maxIter
             maxIter *= 2
             redo_space = False
 
         elif key == ord("d"):
-            # decrease maxiter
+            # Minskar Maxiter, ger färre detaljer.
             lastMaxIter = 0
             maxIter //= 2
             redo_space = False
@@ -160,38 +160,38 @@ if __name__ == "__main__":
             redo_image = False
 
         elif key == ord("c"):
-            # change colormap
+            # Ändrar colormap, vilket ändrar färgerna.
             cMap += 1
             cMap %= 22
             redo_space = False
             redo_iter = False
 
         elif key == ord("z"):
-            # zoom in
+            # Zooma in
             radius /= 2
             print("radius", radius)
             start = center - radius
             stop = center + radius
 
         elif key == ord("x"):
-            # zoom out
+            # Zooma ut
             radius *= 2
             print("radius", radius)
             start = center - radius
             stop = center + radius
 
         elif key == ord("+"):
-            # increase image size
+            # Ökar storleken av bilden på användarens skärm.
             k = int(k * 1.2)
             resolution = np.array([aspectRatio * k, k])
 
         elif key == ord("-"):
-            # decrease image size
+            # Minskar storleken av bilden på användarens skärm.
             k = int(k / 1.2)
             resolution = np.array([aspectRatio * k, k])
 
         elif key == ord("w"):
-            # re-center
+            # Centrerar bilden på användarens skärm, från där användaren klickar.
             c_point = c[coords[-1][1], coords[-1][0]]
             center[0] = c_point.real
             center[1] = c_point.imag
