b38ac5b968ecac95de09356c10935f27f0da437f
Laxen <maximiliam.froste@outlook.com>
2024-04-15 14:56:25 +0200

    Lite cleanup, behövs mer.

--- import cv2.py
+++ import cv2.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-coords = [] # Här sparas koordinaterna som användaren klickar på
+coords = []  # Här sparas koordinaterna som användaren klickar på
 
 
 def onMouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN: # Om vänster musknapp klickas
+    if event == cv2.EVENT_LBUTTONDOWN:  # Om vänster musknapp klickas
         coords.append([x, y])
         print(x, y)
 
@@ -25,7 +25,7 @@ def getComplexPlane(start, stop, num):
 
 
 def f(z, c):
-    return z**2 + c # beräkningen för mandelbrot
+    return z**2 + c  # beräkningen för mandelbrot
 
 
 def applyNTimes(z, c, last_n, n, escape):
@@ -53,110 +53,35 @@ def colourImage(image, mod, cMap=cv2.COLORMAP_TWILIGHT_SHIFTED):
     return image
 
 
-def textOverlay(image, center, radius, maxIter, resolution):
-    # Setup för text
-    h = resolution[1]
-    fontScale = h / 1300
-    fontHeight = 30 * fontScale
-
-    c = np.around(center, 3)
-    if radius[1] < 1:
-        r = [np.format_float_scientific(x, 3, trim="-") for x in radius]
-    else:
-        r = np.around(radius, 3)
-
-    # Lägger till text
-    image = cv2.putText(
-        image,
-        (
-            f"center: ({c[0]}, {c[1]}i)     radius: ({r[0]}, {r[1]})    "
-            + f"resolution: {resolution[0]}x{resolution[1]}    iterations {maxIter}"
-        ),
-        (int(0.5 * fontHeight), int(1.5 * fontHeight)),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        fontScale,
-        (255, 255, 255),
-        thickness=max(1, int(fontScale)),
-    )
-    return image
-
-
-def reCenter(coords, c):
-    # Ändrar vart centern bilden är, beroende på var användaren klickar
-    cPoint = c[coords[-1][1], coords[-1][0]]
-
-    center[0] = cPoint.real
-    center[1] = cPoint.imag
-    start = center - radius
-    stop = center + radius
-
-    return center, start, stop
-
-
-def zoomIn(radius):
-    # Zoomar in
-    radius /= 2
-    start = center - radius
-    stop = center + radius
-
-    return radius, start, stop
-
-
-def zoomOut(radius):
-    # Zoomar ut
-    radius *= 2
-    start = center - radius
-    stop = center + radius
-
-    return radius, start, stop
-
-
-def maxIterUp(maxIter):
-    # Ökar maxiter, vilket ger mer detalj
-    lastMaxIter = maxIter
-    maxIter *= 2
-    return lastMaxIter, maxIter
-
-
-def maxIterDown(maxIter):
-    # Minskar maxiter, vilket ger mindre detalj
-    lastMaxIter = 0
-    maxIter //= 2
-    return lastMaxIter, maxIter
-
-
-def saveImage(image, start, stop):
-    # Sparar bilden som en png
-    xStr = f"{str(start[0]).replace('.','_')}-{str(stop[0]).replace('.','_')}"
-    yStr = f"{str(start[1]).replace('.','_')}-{str(stop[1]).replace('.','_')}"
-
-    # Sparar bilden i samma mapp som programmet
-    cv2.imwrite(f"x-{xStr}-y-{yStr}-m-{maxIter}-c-{cMap}-{mod}.png", image)
-
-
-def rollcMap(cMap):
-    # Ändrar färg karta
-    cMap += 1
-    cMap %= 22
-    return cMap
-
-
-def resolutionUp(k, aspecRatio):
-    # Ökar resolutionen
-    k = int(k * 1.2)
-    resolution = np.array([aspectRatio * k, k], dtype=int)
-    return k, resolution
-
-
-def resolutionDown(k, aspectRatio):
-    # Sänker resolutionen
-    k = int(k / 1.2)
-    resolution = np.array([aspectRatio * k, k], dtype=int)
-    return k, resolution
-
+# def textOverlay(image, center, radius, maxIter, resolution):
+#     # Setup för text
+#     h = resolution[1]
+#     fontScale = h / 1300
+#     fontHeight = 30 * fontScale
+#
+#     c = np.around(center, 3)
+#     if radius[1] < 1:
+#         r = [np.format_float_scientific(x, 3, trim="-") for x in radius]
+#     else:
+#         r = np.around(radius, 3)
+#
+#     # Lägger till text
+#     image = cv2.putText(
+#         image,
+#         (
+#             f"center: ({c[0]}, {c[1]}i)     radius: ({r[0]}, {r[1]})    "
+#             + f"resolution: {resolution[0]}x{resolution[1]}    iterations {maxIter}"
+#         ),
+#         (int(0.5 * fontHeight), int(1.5 * fontHeight)),
+#         cv2.FONT_HERSHEY_SIMPLEX,
+#         fontScale,
+#         (255, 255, 255),
+#         thickness=max(1, int(fontScale)),
+#     )
+#     return image
 
 if __name__ == "__main__":
-    k = 215
+    k = 600
     aspectRatio = 16 / 9
     resolution = np.array([aspectRatio * k, k], dtype=int)
 
