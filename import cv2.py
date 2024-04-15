6154a6ccaabe368921438959501c73b53d39828d
Laxen <maximiliam.froste@outlook.com>
2024-04-15 11:22:48 +0200

    cMap fix

--- import cv2.py
+++ import cv2.py
@@ -1,25 +1,6 @@
 import cv2
 import numpy as np
 
-k = 432
-aspectRatio = 16 / 9
-resolution = np.array([aspectRatio * k, k], dtype=int)
-
-center = np.array([-0.5, 0.0], dtype=float)
-radius = np.array([aspectRatio, 1], dtype=float)
-start = center - radius
-stop = center + radius
-
-maxIter = 40
-lastMaxIter = 0
-mod = 40
-cMap = 19
-
-key = ord("w")
-
-cv2.namedWindow("Mandelbrot")
-
-# Mus koordinater
 coords = []
 
 
@@ -29,9 +10,6 @@ def onMouse(event, x, y, flags, param):
         print(x, y)
 
 
-cv2.setMouseCallback("Mandelbrot", onMouse)
-
-
 def getComplexPlane(start, stop, num):
     # Skaffar arrays från xy arrays
     xSpace = np.linspace(start[0], stop[0], int(num[0]))
@@ -39,7 +17,7 @@ def getComplexPlane(start, stop, num):
     # Meshgrids (vet inte vad det betyder)
     real, imag = np.meshgrid(xSpace, ySpace[::-1])
     # Setting upp the komplex plane
-    c = np.zeros_like(real, dtype = np.cdouble)
+    c = np.zeros_like(real, dtype=np.cdouble)
     c.real = real
     c.imag = imag
 
@@ -50,33 +28,27 @@ def f(z, c):
     return z**2 + c
 
 
-def applyNTimes(z, c, lastN, n, escape, goOn):
-    if not goOn:
-        z = np.zeros_like(z)
-        escape = np.zeros_like(escape)
-
-    # Applicerar mängd gånger
-    for _ in range(lastN, n):
-        # Utför grejsimojsen
+def applyNTimes(z, c, last_n, n, escape):
+    # Apply n times
+    for _ in range(last_n, n):
         z = f(z, c)
-        # Kollar vilka positioner som åker iväg
-        m = np.abs(z) > 2
-        # Increase count for points still in set
+        m = np.abs(z) < 2
         escape[m] += 1
-
-    return z, escape
+        if np.sum(m) == 0:
+            break
+    return escape
 
 
 def colourImage(image, mod, cMap=cv2.COLORMAP_TWILIGHT_SHIFTED):
-    # Mandebrot
+    # Get mandelbrot set
     mask = image == np.max(image)
-    # Modulo bild
+    # Modulo image
     image %= mod
-    # Scalear bilden för shcnasiga grejer
+    # Scale values to 8bit image and convert type
     image = (255 / (mod - 1) * image).astype(np.uint8)
-    # Färga
+    # Colorize
     image = cv2.applyColorMap(image, cMap)
-    # Färgar mandelbrot svart
+    # Color mandelbrot set black
     image[mask] = [0, 0, 0]
     return image
 
@@ -171,38 +143,121 @@ def resolutionDown(k, aspectRatio):
     return k, resolution
 
 
-while True:
-    if chr(key) in "wzx+-":
-        c = getComplexPlane(start, stop, resolution)
-        z = np.zeros_like(c)
-        escape = np.zeros_like(c, dtype=float)
-    if chr(key) in "wzx+-ed":
-        z, escape = applyNTimes(z, c, lastMaxIter, maxIter, escape, chr(key) == "e")
-    if chr(key) in "wzx+-efc":
-        image = colourImage(escape.copy(), mod, cMap)
-        image = textOverlay(image, center, radius, maxIter, resolution)
-        cv2.imshow("Mandelbrot", image)
+if __name__ == "__main__":
+    k = 215
+    aspectRatio = 16 / 9
+    resolution = np.array([aspectRatio * k, k], dtype=int)
 
-    key = cv2.waitKey() & 0xFF
+    center = np.array([-0.5, 0.0], dtype=float)
+    radius = np.array([aspectRatio, 1], dtype=float)
+    start = center - radius
+    stop = center + radius
 
-    if key == ord("q"):  # Quit
-        cv2.destroyAllWindows()
-        break
-    elif key == ord("w"):  # re-center
-        center, start, stop = reCenter(coords, c)
-    elif key == ord("z"):  # zooma in
-        radius, start, stop = zoomIn(radius)
-    elif key == ord("x"):  # zooma ut
-        radius, start, stop = zoomOut(radius)
-    elif key == ord("e"):  # ökar max iter
-        lastMaxIter, maxIter = maxIterUp(maxIter)
-    elif key == ord("d"):  # Sänker max iter
-        lastMaxIter, maxIter = maxIterDown(maxIter)
-    elif key == ord("s"):  # spara
-        saveImage(image, start, stop)
-    elif key == ord("c"):  # ändrar colormap
-        cMap = rollcMap(cMap)
-    elif key == ord("+"):  # öka bild storlek
-        k, resolution = resolutionUp(k, aspectRatio)
-    elif key == ord("-"):  # minskar bild storlek
-        k, resolution = resolutionDown(k, aspectRatio)
+    maxIter = 40
+    lastMaxIter = 0
+    mod = 40
+    cMap = 19
+
+    redoSpace = True
+    redoIter = True
+    redoImage = True
+
+    key = ord("w")
+
+    cv2.namedWindow("Mandelbrot")
+    cv2.setMouseCallback("Mandelbrot", onMouse)
+
+    while True:
+        if redoSpace == True:
+            c = getComplexPlane(start, stop, resolution)
+
+        if redoIter == True:
+            escape = applyNTimes(
+                np.zeros_like(c),
+                c,
+                lastMaxIter,
+                maxIter,
+                np.zeros_like(c, dtype=float),
+            )
+        if redoImage == True:
+            image = colourImage(escape, mod, cMap)
+            cv2.imshow("Mandelbrot", image)
+        print(
+            f"section [[{start[0]},{start[1]}],[{stop[0]},{stop[1]}]]\nmaxiter {maxIter}"
+        )
+        print(f"cmap {cMap}\nres {resolution}\n")
+
+        # Föberedelser för nästa iteration
+        redoSpace = True
+        redoIter = True
+        redoImage = True
+        lastMaxIter = 0
+
+        key = cv2.waitKey() & 0xFF
+
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            break
+        elif key == ord("s"):
+            x_str = f"{str(start[0]).replace('.','_')}-{str(stop[0]).replace('.','_')}"
+            y_str = f"{str(start[1]).replace('.','_')}-{str(stop[1]).replace('.','_')}"
+            cv2.imwrite(
+                f"output/x-{x_str}-y-{y_str}-m-{maxIter}-c-{cMap}-{mod}.png", image
+            )
+            redo_space = False
+            redo_iter = False
+            redo_image = False
+
+        elif key == ord("e"):
+            # extend maxiter
+            lastMaxIter = maxIter
+            maxIter *= 2
+            redo_space = False
+
+        elif key == ord("d"):
+            # decrease maxiter
+            lastMaxIter = 0
+            maxIter //= 2
+            redo_space = False
+            redo_iter = False
+            redo_image = False
+
+        elif key == ord("c"):
+            # change colormap
+            cMap += 1
+            cMap %= 22
+            redo_space = False
+            redo_iter = False
+
+        elif key == ord("z"):
+            # zoom in
+            radius /= 2
+            print("radius", radius)
+            start = center - radius
+            stop = center + radius
+
+        elif key == ord("x"):
+            # zoom out
+            radius *= 2
+            print("radius", radius)
+            start = center - radius
+            stop = center + radius
+
+        elif key == ord("+"):
+            # increase image size
+            k = int(k * 1.2)
+            resolution = np.array([aspectRatio * k, k])
+
+        elif key == ord("-"):
+            # decrease image size
+            k = int(k / 1.2)
+            resolution = np.array([aspectRatio * k, k])
+
+        elif key == ord("w"):
+            # re-center
+            c_point = c[coords[-1][1], coords[-1][0]]
+            center[0] = c_point.real
+            center[1] = c_point.imag
+            start = center - radius
+            stop = center + radius
+            print("center", center[0], center[1])
